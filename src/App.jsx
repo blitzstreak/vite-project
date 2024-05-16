e3ad1da697441fc09fd8de110309f884f968c83a
Andy Yu <andy.chenyuhuang@gmail.com>
2024-05-16 09:58:04 -0400

    newcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import { puppyList } from './data.js';
-import './App.css';
+import { useState } from 'react'
+import { puppyList } from './data.js'
+import './index.css'
 
 function App() {
   const [featPupId, setFeatPupId] = useState(null);
-  const [puppies, setPuppies] = useState(puppyList);
+  const [puppies] = useState(puppyList);
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  
   console.log(featuredPup);
   return (
-    <>
-      <div>
+       <div className="App">
         {featPupId && (
-          <div className='pupDetails'>
+          <div>
             <h2>{featuredPup.name}</h2>
             <ul>
               <li>Age: {featuredPup.age}</li>
@@ -19,48 +19,13 @@ function App() {
             </ul>
           </div>
         )}
-        {puppies.map((puppy) => {
-          return (
-            <p
-              onClick={() => {
-                setFeatPupId(puppy.id);
-              }}
-              key={puppy.id}
-            >
-              {puppy.name}
-            </p>
-          );
-        })}
-      </div>
-    </>
-  );
-}
+          {
+          puppies.map((puppy) => {
+               return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+             })
+         }
+      </div>        
+    );
+  }
 
 export default App
-
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
